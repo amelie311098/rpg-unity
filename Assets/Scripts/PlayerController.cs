5605d6db8a79ca956d19f679b11d1759c9528647
Amelie Bertin <amelie.bertin@epita.fr>
2020-10-12 12:02:20 +0300

    Scripts: add displacement to player: 1st person view

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 3f;
+    private Rigidbody rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Turn();
+    }
+
+    private void Move()
+    {
+        Vector3 relative = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.I))
+        {
+            // forward
+            relative += transform.InverseTransformDirection(Vector3.forward).normalized * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.J))
+        {
+            // left
+            relative += transform.InverseTransformDirection(Vector3.left).normalized * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.K))
+        {
+            // backward
+            relative += transform.InverseTransformDirection(Vector3.back).normalized * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.L))
+        {
+            // right
+            relative += transform.InverseTransformDirection(Vector3.right).normalized * speed * Time.deltaTime;
+        }
+
+        rigidbody.MovePosition(transform.position + relative);
+    }
+
+    private void Turn()
+    {
+
+    }
+}
