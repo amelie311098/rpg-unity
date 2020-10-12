f13dd6063ac086dc254a47cafc789b732de51a78
Amelie Bertin <amelie.bertin@epita.fr>
2020-10-12 12:26:41 +0300

    Script: add rotation displacement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private float speed = 3f;
+    private float speed_rotation = 20f;
     private Rigidbody rigidbody;
 
     // Start is called before the first frame update
@@ -27,29 +28,40 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKey(KeyCode.I))
         {
             // forward
-            relative += transform.InverseTransformDirection(Vector3.forward).normalized * speed * Time.deltaTime;
+            relative += transform.forward * speed * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.J))
         {
             // left
-            relative += transform.InverseTransformDirection(Vector3.left).normalized * speed * Time.deltaTime;
+            relative += transform.right * -1 * speed * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.K))
         {
             // backward
-            relative += transform.InverseTransformDirection(Vector3.back).normalized * speed * Time.deltaTime;
+            relative += transform.forward * -1 * speed * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.L))
         {
             // right
-            relative += transform.InverseTransformDirection(Vector3.right).normalized * speed * Time.deltaTime;
+            relative += transform.right * speed * Time.deltaTime;
         }
 
-        rigidbody.MovePosition(transform.position + relative);
+        rigidbody.MovePosition(rigidbody.position + relative);
     }
 
     private void Turn()
     {
-
+        float direction = 0;
+        if (Input.GetKey(KeyCode.A))
+        {
+            direction = -1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            direction = 1;
+        }
+        float turn = direction * speed_rotation * Time.deltaTime;
+        Quaternion rotation = Quaternion.Euler(0, turn, 0);
+        rigidbody.MoveRotation(rigidbody.rotation * rotation);
     }
 }
